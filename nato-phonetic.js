b9ed4f9937835fc629db1e528a8355daeda4a60e
Ben Chopson <bchopson@users.noreply.github.com>
2022-11-12 16:56:29 -0500

    Add functions

---
+++ nato-phonetic.js
@@ -0,0 +1,35 @@
+function toNatoMessage(text) {
+  return [...text].map(lookupNato).reduce((acc, curr) => acc + ' ' + curr);
+}
+
+const lookupNato = (c) => {
+  const natoAlphabet = {
+    A: 'alfa',
+    B: 'bravo',
+    C: 'charlie',
+    D: 'delta',
+    E: 'echo',
+    F: 'foxtrot',
+    G: 'golf',
+    H: 'hotel',
+    I: 'india',
+    J: 'juliett',
+    K: 'kilo',
+    L: 'lima',
+    M: 'mike',
+    N: 'november',
+    O: 'oscar',
+    P: 'papa',
+    Q: 'quebec',
+    R: 'romeo',
+    S: 'sierra',
+    T: 'tango',
+    U: 'uniform',
+    V: 'victor',
+    W: 'whiskey',
+    X: 'xray',
+    Y: 'yankee',
+    Z: 'zulu',
+  };
+  return natoAlphabet[c.toUpperCase()] || c;
+}
